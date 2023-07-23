a68e9e9dbcec9fd7d34ff42ef88cfa7708e6cbd1
Vinicius Souza <viniciusdyo@gmail.com>
2023-07-23 15:03:21 -0300

    Adicionar arquivos de projeto.

---
+++ JwtAPILecture/Controllers/TeamsController.cs
@@ -0,0 +1,84 @@
+﻿using JwtAPILecture.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAPILecture.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TeamsController : ControllerBase
+{
+    private static List<Team> teams = new List<Team>()
+    {
+        new Team()
+        {
+            Id = 1,
+            Country = "Brasil",
+            Name = "Test",
+            TeamPrinciple = "Vini Boladao"
+        },
+        new Team()
+        {
+            Id = 2,
+            Country = "Italy",
+            Name = "Test2",
+            TeamPrinciple = "Vini nao tao bolado"
+        },
+        new Team()
+        {
+            Id = 3,
+            Country = "Germany",
+            Name = "Test3",
+            TeamPrinciple = "Vini boladinho"
+        }
+    };
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(teams);
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult Get(int id)
+    {
+        var team = teams.FirstOrDefault(t => t.Id == id);
+
+        if (team == null)
+            return BadRequest("Invalid Id");
+        
+        return Ok(team);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Team team)
+    {
+        teams.Add(team);
+
+        return CreatedAtAction("Get", team.Id, team);
+    }
+
+    [HttpPatch]
+    public IActionResult Patch(int id, string country)
+    {
+        var team = teams.FirstOrDefault(x => x.Id == id);
+
+        if (team == null) return BadRequest("Invalid Id");
+
+        team.Country= country;
+
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        var team = teams.FirstOrDefault(x => x.Id == id);
+        if (team == null) return BadRequest("Invalid Id");
+
+        teams.Remove(team);
+
+        return NoContent();
+    }
+}
+
---
+++ JwtAPILecture/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtAPILecture.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string TeamPrinciple { get; set; }
+    }
+}
