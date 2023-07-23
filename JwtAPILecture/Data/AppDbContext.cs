b33692fb69be08d2cdeab853d60c82a104dfe649
Vinicius Souza <viniciusdyo@gmail.com>
2023-07-23 15:21:40 -0300

    - Create DbContext for SQL Server - Change SQLite to SQL Server correct package

---
+++ JwtAPILecture/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JwtAPILecture.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAPILecture.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
