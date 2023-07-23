0485a4c9e626c5f681a5c94544ccd0d1cdc17d11
Vinicius Souza <viniciusdyo@gmail.com>
2023-07-23 16:10:04 -0300

    - Added DbContext for SQL Server - Change the in memory methods in controller to in database

--- JwtAPILecture/Controllers/TeamsController.cs
+++ JwtAPILecture/Controllers/TeamsController.cs
@@ -1,6 +1,8 @@
-﻿using JwtAPILecture.Models;
+﻿using JwtAPILecture.Data;
+using JwtAPILecture.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JwtAPILecture.Controllers;
 
@@ -8,75 +10,87 @@ namespace JwtAPILecture.Controllers;
 [ApiController]
 public class TeamsController : ControllerBase
 {
-    private static List<Team> teams = new List<Team>()
+    private static AppDbContext _context;
+    public TeamsController(AppDbContext context)
     {
-        new Team()
-        {
-            Id = 1,
-            Country = "Brasil",
-            Name = "Test",
-            TeamPrinciple = "Vini Boladao"
-        },
-        new Team()
-        {
-            Id = 2,
-            Country = "Italy",
-            Name = "Test2",
-            TeamPrinciple = "Vini nao tao bolado"
-        },
-        new Team()
-        {
-            Id = 3,
-            Country = "Germany",
-            Name = "Test3",
-            TeamPrinciple = "Vini boladinho"
-        }
-    };
+        _context = context;
+    }
+
+    //private static List<Team> teams = new List<Team>()
+    //{
+    //    new Team()
+    //    {
+    //        Id = 1,
+    //        Country = "Brasil",
+    //        Name = "Test",
+    //        TeamPrinciple = "Vini Boladao"
+    //    },
+    //    new Team()
+    //    {
+    //        Id = 2,
+    //        Country = "Italy",
+    //        Name = "Test2",
+    //        TeamPrinciple = "Vini nao tao bolado"
+    //    },
+    //    new Team()
+    //    {
+    //        Id = 3,
+    //        Country = "Germany",
+    //        Name = "Test3",
+    //        TeamPrinciple = "Vini boladinho"
+    //    }
+    //};
 
     [HttpGet]
-    public IActionResult Get()
+    public async Task<IActionResult> Get()
     {
+        var teams = await _context.Teams.ToListAsync();
+
         return Ok(teams);
     }
 
     [HttpGet("{id:int}")]
-    public IActionResult Get(int id)
+    public async Task<IActionResult> Get(int id)
     {
-        var team = teams.FirstOrDefault(t => t.Id == id);
+        var team = await _context.Teams.FirstOrDefaultAsync(t => t.Id == id);
 
         if (team == null)
             return BadRequest("Invalid Id");
-        
+
         return Ok(team);
     }
 
     [HttpPost]
-    public IActionResult Post(Team team)
+    public async Task<IActionResult> Post(Team team)
     {
-        teams.Add(team);
+        await _context.Teams.AddAsync(team);
+        await _context.SaveChangesAsync();
 
         return CreatedAtAction("Get", team.Id, team);
     }
 
     [HttpPatch]
-    public IActionResult Patch(int id, string country)
+    public async Task<IActionResult> Patch(int id, string country)
     {
-        var team = teams.FirstOrDefault(x => x.Id == id);
+        var team = await _context.Teams.FirstOrDefaultAsync(x => x.Id == id);
 
         if (team == null) return BadRequest("Invalid Id");
-
-        team.Country= country;
+        
+        team.Country = country;
+        _context.Teams.Update(team);
+        await _context.SaveChangesAsync();
 
         return NoContent();
     }
 
     [HttpDelete]
-    public IActionResult Delete(int id)
+    public async Task<IActionResult> Delete(int id)
     {
-        var team = teams.FirstOrDefault(x => x.Id == id);
+        var team = await _context.Teams.FirstOrDefaultAsync(x => x.Id == id);
         if (team == null) return BadRequest("Invalid Id");
 
-        teams.Remove(team);
+        _context.Teams.Remove(team);
+        await _context.SaveChangesAsync();
 
         return NoContent();
     }
